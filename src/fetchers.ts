8553f96ea7a53ea63596e229d3d1f4d8766c7ef5
frank renato <frankfigao@gmail.com>
2023-06-01 11:44:59 -0600

    Removing unneeded file

--- src/fetchers.ts
+++
@@ -1,9 +0,0 @@
-export const fetcherData = async (url: string) => {
-  try {
-    const rsp = await axios.get(url)
-    if (rsp.data.status === 'fail') return [rsp.data, null]
-    return [null, rsp.data]
-  } catch (error) {
-    return [error, null]
-  }
-}
